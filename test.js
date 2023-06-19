42d8a9a9f11ab5e1084bd2716d321fe954f4660b
TsvetiIvanov <136731967+TsvetiIvanov@users.noreply.github.com>
2023-06-20 00:43:58 +0300

    Add files via upload

--- test.js
+++ test.js
@@ -2,79 +2,103 @@
 const workplaceNames = {
   'CMF1': {
     dropdownCount: 3,
-    names: [` `,'Sashka Z', 'Sashka K', 'Silvia I']
+    names: [` `,`Sashka Z`, `Sashka K`, `Silvia I`]
   },
   'VolvoBB-CU': {
     dropdownCount: 1,
-    names: [` `,'Sashka Z', 'Magdalena']
+    names: [` `,`Sashka Z`, `Magdalena`]
   },
   'VolvoSpa': {
     dropdownCount: 1,
-    names: [` `,'Sashka Z', 'Nadejda', 'Sashka K']
-  },
-  'BR 1': {
-    dropdownCount: 2,
-    names: [` `,'Zvezdelina', 'Silvia I', 'Nadejda']
-  },
-  'BR 2': {
-    dropdownCount: 1,
-    names: [` `,'Silvia L', 'Silvia I', 'Nadejda']
-  },
-  'Scania': {
-    dropdownCount: 1,
-    names: [` `,'Elza', 'Vanq']
-  },
-  'PTC35UP': {
-    dropdownCount: 1,
-    names: [` `,'Sashka Z', 'Sashka K', 'Valeri']
+    names: [` `,`Sashka Z`, `Nadejda`, `Sashka K`, `Magi`]
   },
   'Man BB/CU': {
     dropdownCount: 1,
-    names: [` `,'Sashka K', 'Elena E']
+    names: [` `,`Sashka K`, `Elena E`]
   },
-  'MQB CLT 1': {
-    dropdownCount: 3,
-    names: [` `,'Koce', 'Misho']
-  },
-  'MQB CLT 2': {
-    dropdownCount: 3,
-    names: [` `,'Koce', 'Misho']
-  },
-  'MNB CLT 3': {
+  'BR 1': {
     dropdownCount: 2,
-    names: [` `,'Silvia I', 'Olivia']
+    names: [` `, `Zvezdelina`, `Silvia I`, `Nadejda`, `Nina`]
   },
-  'Touareg': {
+  'BR 2': {
     dropdownCount: 1,
-    names: [` `,'Rumqna', 'Rayna']
-  },
-  'MLBevo': {
-    dropdownCount: 1,
-    names: [` `,'Sashka Z', 'Sashka K']
-  },
-  'Audi Heck': {
-    dropdownCount: 4,
-    names: [` `,'Rayna', 'Silvia M', 'Iliyana', `Vili`]
-  },
-  'MQB 37': {
-    dropdownCount: 2,
-    names: [` `,'Elena E', 'Emilia']
-  },
-  'MEB 31': {
-    dropdownCount: 2,
-    names: [` `,'Magdalena', 'Nina']
+    names: [` `,`Silvia L`, `Silvia I`, `Nadejda`, `Nina`]
   },
   'MFA2': {
     dropdownCount: 3,
-    names: [` `,'Danislava', 'Lili', 'Dimitar']
+    names: [` `, `Danislava`, `Lili`, `Dimitar`, `Ali`]
+  },
+  'Scania': {
+    dropdownCount: 1,
+    names: [` `, `Elza`, `Vanq`]
+  },
+  'PTC35UP': {
+    dropdownCount: 1,
+    names: [` `, `Sashka Z`, `Sashka K`, `Valeri`]
+  },
+  'MQB Heck' : {
+    dropdownCount: 5,
+    names: [` `, `Valeri`, `Koce`, `Elena E`]
+  },
+  'MQB 27' : {
+    dropdownCount: 3,
+    names: [` `, `Rumi`, `Sevdelin`, `Angel`, `Zvezdelina`, `Olivera`]
+  },
+  'MQB CLT 1': {
+    dropdownCount: 3,
+    names: [` `, `Koce`, `Misho`, `Danislava`, `Olivera`, `Sasho`]
+  },
+  'MQB CLT 2': {
+    dropdownCount: 3,
+    names: [` `, `Koce`, `Misho`, `Danislava`, `Olivera`, `Sasho`]
+  },
+  'MNB CLT 3': {
+    dropdownCount: 2,
+    names: [` `, `Silvia I`, `Olivera`, `Sasho`]
+  },
+  'MQB CLT 5': {
+    dropdownCount: 1,
+    names: [` `, `Sasho`]
+  },
+  'Touareg': {
+    dropdownCount: 1,
+    names: [` `, `Rumqna`, `Rayna`]
+  },
+  'MLBevo': {
+    dropdownCount: 1,
+    names: [` `, `Sashka Z`, `Sashka K`]
+  },
+  'Audi Heck': {
+    dropdownCount: 4,
+    names: [` `, `Rayna`, `Silvia M`, `Iliyana`, `Vili`, `Emilia`]
+  },
+  'MQB 37': {
+    dropdownCount: 2,
+    names: [` `, `Elena E`, `Emilia`, `Magdalena`, `Nina`]
+  },
+  'MEB 31': {
+    dropdownCount: 2,
+    names: [` `, `Magdalena`, `Nina`, `Elena E`, `Emilia`]
+  },
+  'ITOS 1': {
+    dropdownCount: 4,
+    names: [` `, `Elena M`, `Rumqna`, `Katq`, `Marinela`, `Vanq`, `Elena K`, `Nadejda`]
+  },
+  'ITOS 2': {
+    dropdownCount: 4,
+    names: [` `, `Elena M`, `Rumqna`, `Katq`, `Marinela`, `Vanq`, `Elena K`, `Nadejda`]
+  },
+  'MRA2 Rear': {
+    dropdownCount: 2,
+    names: [` `, `Marinela`, ` Vanq`, `Silvia M`, `Valeri` ]
   },
   'MQB CLT Touch': {
     dropdownCount: 4,
-    names: [` `,'Svetla', 'Rosi', 'Stefi',`Vanq`]
+    names: [` `, `Svetla`, `Rosi`, `Stefi`, `Vanq`, `Valeri`]
   },
   'MQB 27 Touch': {
     dropdownCount: 4,
-    names: [` `,'Vili', 'Elena K', 'Olivera',`Valeri`]
+    names: [` `, `Vili`, `Elena K`, `Olivera`, `Valeri`, `Tatqna`, `Donka`]
   }
   // Add more workplaces and names here
 };
